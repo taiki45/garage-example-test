00f56061ac351b22830b4b5e5bceffc4c10472d3
Taiki ONO <taiki-ono@cookpad.com>
2014-09-22 20:48:43 +0900

    Add user resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  include Garage::Representer
+  include Garage::Authorizable
+
+  class << self
+    # Any user resources are public
+    def build_permissions(perms, other, target)
+      perms.permits! :read, :write
+    end
+  end
+
+  property :id
+  property :name
 end
